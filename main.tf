f892b3b3de7a2f36a97da5e1a24e7954aba0ec33
Mark <elpicsid@gmail.com>
2023-05-23 01:48:30 -0500

    rebuild

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform{
+    required_providers{
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "3.56.0"
+        }
+    }
+}
+
+provider "azurerm"{
+    features{
+      
+    }
+}
+
+resource "azurerm_resource_group" "tf_rg_mywebapp" {
+  name = var.iac_resource_group
+  location = "eastus"
+
+}
+
+resource "azurerm_service_plan" "tf_service_plan" {
+ name                = "mywebapp_sp"
+ sku_name            = "S1"
+ location            = "eastus"
+ resource_group_name = azurerm_resource_group.tf_rg_mywebapp.name
+ os_type             = "Linux"
+}
+
+resource "azurerm_linux_web_app" "tf_mywebapp_app_service" {
+  name                = var.iac_linux_web_app
+  location            = azurerm_resource_group.tf_rg_mywebapp.location
+  resource_group_name = azurerm_resource_group.tf_rg_mywebapp.name
+  service_plan_id     = azurerm_service_plan.tf_service_plan.id
+
+  
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL = var.iac_docker_url
+    DOCKER_REGISTRY_SERVER_USERNAME = var.iac_docker_un
+    DOCKER_REGISTRY_SERVER_PASSWORD = var.iac_docker_pw
+  }
+
+  site_config {
+    #always_on = false
+    application_stack {
+      docker_image = "juuniper/mywebapp"
+	    docker_image_tag = "latest"
+    }
+  }
+}
