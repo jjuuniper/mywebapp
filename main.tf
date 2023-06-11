97fe1440fd893e13571563b9c88f8adc82bff321
Mark <elpicsid@gmail.com>
2023-06-11 16:53:06 -0500

    moving vars back to var file and updating yaml command

--- main.tf
+++ main.tf
@@ -13,16 +13,6 @@ provider "azurerm"{
     }
 }
 
-variable "image_build" {
-  type = string
-  description = "latest build version"
-}
-
-variable "iac_resource_group" {
-  type = string
-  default = "jjuunipertfrg"
-}
-
 resource "azurerm_resource_group" "tf_rg_mywebapp" {
   name = var.iac_resource_group
   location = "eastus"
